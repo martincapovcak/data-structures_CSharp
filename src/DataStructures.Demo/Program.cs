0620f29d9a1e5655155b9b023ecfe712d78a33ac
Martin <martin.capovcak@gmail.com>
2025-05-24 23:07:20 +0200

    refactor and Demo setup

--- src/DataStructures.Demo/Program.cs
+++ src/DataStructures.Demo/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace DataStructures.Demo;
+﻿
+using DataStructures.LinkedLists;
+
+namespace DataStructures.Demo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        MyLinkedList<int> l = new MyLinkedList<int>();
+        l.AddLast(10);
+        l.AddLast(20);
+        l.AddLast(30);
+        l.AddFirst(40);
+        l.Display();
     }
 }
---
+++ src/DataStructures/LinkedLists/MyLinkedList.cs
@@ -0,0 +1,99 @@
+namespace DataStructures.LinkedLists;
+
+// Generic class for flexibility
+public class Node<T>
+{
+    // Holds the node's data
+    public T Element { get; set; }
+
+    // Points to the next node
+    // ? Nullable reference (used for last node)
+    public Node<T>? Next { get; set; }
+
+    // Sets the value and initializes Next to null
+    public Node(T element)
+    {
+
+        Element = element;
+        Next = null;
+    }
+}
+
+public class MyLinkedList<T>
+{
+    private Node<T>? head;
+    private Node<T>? tail;
+    private int count;
+
+    public int Count { get => count; }
+    public bool IsEmpty { get => count == 0; }
+
+    public MyLinkedList()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    #region Add Methods         // Create
+
+    public void AddLast(T element)
+    {
+        Node<T> node = new Node<T>(element);
+
+        if (this.IsEmpty)
+            head = node;
+        else
+            tail.Next = node;
+
+        tail = node;
+        count++;
+    }
+
+    public void AddFirst(T element)
+    {
+        Node<T> node = new Node<T>(element);
+
+        if (this.IsEmpty)
+            tail = node;
+        else
+            node.Next = head;
+
+        head = node;
+        count++;
+    }
+
+    #endregion
+
+    #region Query Methods       // Read
+    #endregion
+
+    #region Delete Methods      // Delete
+    #endregion
+
+    #region Structure Methods   // Manipulation
+    #endregion
+
+    #region Utility Methods     // Debug / Export
+
+    public void Display()
+    {
+        Node<T> p = head;
+
+        Console.Write("Linked List ");
+        Console.Write("{ ");
+
+        while (p != null)
+        {
+            Console.Write($"v: {p.Element}");
+            Console.Write(" --> ");
+
+            p = p.Next;
+        }
+
+        Console.Write(" }");
+        Console.WriteLine();
+    }
+
+    #endregion
+}
